de7ef1c3885aacc011dd873b8a43ec1c3a55a7c1
tannerkrewson <tannerkrewson@gmail.com>
2015-08-21 13:28:04 -0500

    Added key press events

--- public_html/js/computerbowling.js
+++ public_html/js/computerbowling.js
@@ -1,7 +1,38 @@
-document.getElementById('bowling').value = "0001357975310000000000000000000000000000000000000000000000000000000000000";
-readOnly(true);
+refillBox();
+readOnly(false);
 
+var blocker = false;
+var rolling = true;
+
+function start(){
+    readOnly(false);
+    blocker = false;
+}
+
+function keyDown(event){
+    if (blocker || event.keyCode !== 8){
+        refillBox();
+        return;
+    }
+    rolling = true;
+}
+
+function keyUp(event){
+    if (blocker || !rolling || event.keyCode !== 8){
+        refillBox();
+        return;
+    }
+    var score;
+    score = document.getElementById('bowling').value;
+    alert(score);
+    refillBox();
+    rolling = false;
+}
 
 function readOnly(tf){
     document.getElementById('bowling').readOnly = tf;
+}
+
+function refillBox(){
+    document.getElementById('bowling').value = "0000013579753100000000000000000000000000000000000000000000000000000000";
 }
